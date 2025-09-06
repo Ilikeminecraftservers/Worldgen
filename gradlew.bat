0208cd752dd82679f3b715e3905772fbdcead813
Ilikeminecraftservers <tobiasgirstl@gmail.com>
2025-09-06 11:56:19 -0500

    Create gradlew.bat

---
+++ gradlew.bat
@@ -0,0 +1,6 @@
+@echo off
+rem ----------------------------------------------------------------------
+rem Gradle start up script for Windows
+rem ----------------------------------------------------------------------
+
+java -jar gradle\wrapper\gradle-wrapper.jar %*
