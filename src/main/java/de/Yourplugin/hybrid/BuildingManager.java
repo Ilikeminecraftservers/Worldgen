c757c0e94d781004638ab59bbbc171418df03d2b
Ilikeminecraftservers <tobiasgirstl@gmail.com>
2025-09-09 11:33:43 -0500

    Aktualisieren von BuildingManager.java

--- src/main/java/de/Yourplugin/hybrid/BuildingManager.java
+++ src/main/java/de/Yourplugin/hybrid/BuildingManager.java
@@ -1,48 +1,37 @@
-package de.Yourplugin.hybrid;
+package de.yourplugin.hybrid;
 
 import org.bukkit.Location;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class BuildingManager {
 
-    // Key = Adresse (z. B. "Hauptstraße 12"), Value = BuildingData
     private final Map<String, BuildingData> buildings = new HashMap<>();
 
-    /**
-     * Gebäude hinzufügen
-     */
     public void addBuilding(String address, Location location, String owner, double price) {
         buildings.put(address, new BuildingData(location, owner, price));
     }
 
-    /**
-     * Gebäude abrufen
-     */
     public BuildingData getBuilding(String address) {
         return buildings.get(address);
     }
 
-    /**
-     * Alle Gebäude abrufen
-     */
-    public Collection<BuildingData> getAllBuildings() {
-        return buildings.values();
-    }
-
-    /**
-     * Gebäude entfernen
-     */
     public void removeBuilding(String address) {
         buildings.remove(address);
     }
 
-    /**
-     * Prüfen ob ein Gebäude existiert
-     */
     public boolean hasBuilding(String address) {
         return buildings.containsKey(address);
+    }
+
+    public Collection<BuildingData> getAllBuildings() {
+        return Collections.unmodifiableCollection(buildings.values());
+    }
+
+    public Map<String, BuildingData> getAllBuildingsMap() {
+        return Collections.unmodifiableMap(buildings);
     }
 }
