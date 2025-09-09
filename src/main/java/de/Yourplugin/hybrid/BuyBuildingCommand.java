b02b65a99a4b9414ff45e6efafbbb3f516e8ea30
Ilikeminecraftservers <tobiasgirstl@gmail.com>
2025-09-09 11:31:20 -0500

    Aktualisieren von BuyBuildingCommand.java

--- src/main/java/de/Yourplugin/hybrid/BuyBuildingCommand.java
+++ src/main/java/de/Yourplugin/hybrid/BuyBuildingCommand.java
@@ -1,4 +1,4 @@
-package de.Yourplugin.hybrid;
+package de.yourplugin.hybrid;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -9,37 +9,39 @@ import org.bukkit.entity.Player;
 
 public class BuyBuildingCommand implements CommandExecutor {
 
-    private final HybridSurvivalPluginConfig plugin;
+    private final HybridSurvivalConfigPlugin plugin;
     private final BuildingManager buildingManager;
 
-    public BuyBuildingCommand(HybridSurvivalPluginConfig plugin) {
+    public BuyBuildingCommand(HybridSurvivalConfigPlugin plugin) {
         this.plugin = plugin;
         this.buildingManager = plugin.getBuildingManager();
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player player)) {
-            sender.sendMessage("Nur Spieler können Gebäude kaufen.");
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "Nur Spieler können Gebäude kaufen.");
             return true;
         }
+
+        Player player = (Player) sender;
 
         if (args.length < 1) {
-            player.sendMessage(ChatColor.RED + "Benutzung: /buybuilding <Adresse>");
+            player.sendMessage(ChatColor.YELLOW + "Benutzung: /buybuilding <Adresse>");
             return true;
         }
 
-        String address = args[0];
+        String address = String.join(" ", args);
         Location loc = player.getLocation();
 
         if (buildingManager.hasBuilding(address)) {
             BuildingData existing = buildingManager.getBuilding(address);
-            player.sendMessage(ChatColor.RED + "Dieses Gebäude gehört bereits " + existing.getOwner() + ".");
+            player.sendMessage(ChatColor.RED + "Dieses Gebäude gehört bereits " + (existing.getOwner() == null ? "Niemand" : existing.getOwner()) + ".");
             return true;
         }
 
         buildingManager.addBuilding(address, loc, player.getName(), 1000.0);
-        player.sendMessage(ChatColor.GREEN + "Du hast das Gebäude bei '" + address + "' erfolgreich gekauft!");
+        player.sendMessage(ChatColor.GREEN + "Du hast das Gebäude '" + address + "' gekauft.");
         return true;
     }
 }
