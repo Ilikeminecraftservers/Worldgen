a3b56aef9c129ee7f525162d8460304a35940e3c
Ilikeminecraftservers <tobiasgirstl@gmail.com>
2025-09-09 11:34:18 -0500

    Aktualisieren von DecorationManager.java

--- src/main/java/de/Yourplugin/hybrid/DecorationManager.java
+++ src/main/java/de/Yourplugin/hybrid/DecorationManager.java
@@ -1,69 +1,42 @@
 package de.yourplugin.hybrid;
 
-import com.sk89q.worldedit.bukkit.BukkitAdapter;
-import com.sk89q.worldedit.extent.clipboard.Clipboard;
-import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat;
-import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormats;
-import com.sk89q.worldedit.extent.clipboard.io.ClipboardReader;
-import com.sk89q.worldedit.session.ClipboardHolder;
 import com.sk89q.worldedit.EditSession;
 import com.sk89q.worldedit.WorldEdit;
-import org.bukkit.Location;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormat;
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormats;
+import com.sk89q.worldedit.function.operation.Operation;
+import com.sk89q.worldedit.function.operation.Operations;
 import org.bukkit.World;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.ByteArrayInputStream;
 
 public class DecorationManager {
 
-    private final boolean lampsEnabled;
-    private final boolean treesEnabled;
-    private final boolean benchesEnabled;
-    private final String lampPath;
-    private final String treePath;
-    private final String benchPath;
+    // Konstruktor nimmt config oder plugin falls benötigt
+    public DecorationManager(/* config param if needed */) { }
 
-    public DecorationManager(org.bukkit.configuration.file.FileConfiguration cfg) {
-        lampsEnabled = cfg.getBoolean("decorations.street-lamps.enabled", false);
-        treesEnabled = cfg.getBoolean("decorations.trees.enabled", false);
-        benchesEnabled = cfg.getBoolean("decorations.benches.enabled", false);
-        lampPath = cfg.getString("decorations.street-lamps.schematic-path", "schematics/lamps/");
-        treePath = cfg.getString("decorations.trees.schematic-path", "schematics/trees/");
-        benchPath = cfg.getString("decorations.benches.schematic-path", "schematics/benches/");
-    }
-
-    public void placeLamp(World world, Location loc) {
-        if (!lampsEnabled) return;
-        pasteSchematic(new File(lampPath + "lamp.schem"), world, loc);
-    }
-
-    public void placeTree(World world, Location loc) {
-        if (!treesEnabled) return;
-        pasteSchematic(new File(treePath + "tree.schem"), world, loc);
-    }
-
-    public void placeBench(World world, Location loc) {
-        if (!benchesEnabled) return;
-        pasteSchematic(new File(benchPath + "bench.schem"), world, loc);
-    }
-
-    private void pasteSchematic(File file, World world, Location loc) {
-        ClipboardFormat format = ClipboardFormats.findByFile(file);
-        if (format == null) return;
-        try (ClipboardReader reader = format.getReader(new FileInputStream(file))) {
-            Clipboard clipboard = reader.read();
-            ClipboardHolder holder = new ClipboardHolder(clipboard);
-            try (EditSession editSession = WorldEdit.getInstance().newEditSession(BukkitAdapter.adapt(world))) {
-                holder.createPaste(editSession)
-                        .to(BukkitAdapter.asBlockVector(loc))
-                        .ignoreAirBlocks(false)
-                        .build()
-                        .paste();
-                editSession.flushQueue();
+    public void pasteSchematic(World world, byte[] data, int x, int y, int z) {
+        try {
+            ClipboardFormat format = ClipboardFormats.findByFile(new ByteArrayInputStream(data));
+            if (format == null) return;
+            try (com.sk89q.worldedit.extent.clipboard.io.ClipboardReader reader = format.getReader(new ByteArrayInputStream(data))) {
+                com.sk89q.worldedit.extent.clipboard.Clipboard clipboard = reader.read();
+                com.sk89q.worldedit.session.ClipboardHolder holder = new com.sk89q.worldedit.session.ClipboardHolder(clipboard);
+                try (EditSession editSession = WorldEdit.getInstance().newEditSession(com.sk89q.worldedit.bukkit.BukkitAdapter.adapt(world))) {
+                    Operation op = holder.createPaste(editSession)
+                            .to(com.sk89q.worldedit.math.BlockVector3.at(x, y, z))
+                            .ignoreAirBlocks(false)
+                            .build();
+                    Operations.complete(op); // statt .paste()
+                }
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    // Beispiel wrapper für later
+    public void placeLamp(World world, org.bukkit.Location loc) {
+        // implement using pasteSchematic or direct block placement
     }
 }
